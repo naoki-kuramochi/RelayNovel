73e2a91decb5db190436614165da70c2be55550a
osechiman <osechiman@gmail.com>
2017-08-27 02:38:29 +0900

    ingress設定調整

--- Kubernetes/create.sh
+++ Kubernetes/create.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-kubectl create -f ingress/ingress.yaml && \
 kubectl create -f senteces-api/deployment.yaml && \
 kubectl create -f novels-api/deployment.yaml && \
 kubectl create -f mysql-proxy/deployment.yaml && \
@@ -7,4 +6,5 @@ kubectl create -f nginx-routing/deployment.yaml && \
 kubectl create -f senteces-api/service.yaml && \
 kubectl create -f novels-api/service.yaml && \
 kubectl create -f mysql-proxy/service.yaml && \
-kubectl create -f nginx-routing/service.yaml
+kubectl create -f nginx-routing/service.yaml && \
+kubectl create -f ingress/ingress.yaml
