ba8bfd84864f0af8119823ea7f10dfb149fc9fba
osechiman <osechiman@gmail.com>
2017-12-17 00:30:06 +0900

    Mod. DBの接続周りを修正

--- Kubernetes/deploy-yaml.sh
+++ Kubernetes/deploy-yaml.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-sed -e "s/COMMIT_SHA/`git rev-parse feature/k8s`/g" status-api/deployment-template.yaml > status-api/deployment.yaml
-sed -e "s/COMMIT_SHA/`git rev-parse feature/k8s`/g" mysql-proxy/deployment-template.yaml > mysql-proxy/deployment.yaml
-sed -e "s/COMMIT_SHA/`git rev-parse feature/k8s`/g" nginx-routing/deployment-template.yaml > nginx-routing/deployment.yaml
-sed -e "s/COMMIT_SHA/`git rev-parse feature/k8s`/g" novels-api/deployment-template.yaml > novels-api/deployment.yaml
-sed -e "s/COMMIT_SHA/`git rev-parse feature/k8s`/g" senteces-api/deployment-template.yaml > senteces-api/deployment.yaml
-sed -e "s/COMMIT_SHA/`git rev-parse feature/k8s`/g" novelists-api/deployment-template.yaml > novelists-api/deployment.yaml
+sed -e "s/COMMIT_SHA/`git rev-parse origin/feature/k8s`/g" status-api/deployment-template.yaml > status-api/deployment.yaml
+sed -e "s/COMMIT_SHA/`git rev-parse origin/feature/k8s`/g" mysql-proxy/deployment-template.yaml > mysql-proxy/deployment.yaml
+sed -e "s/COMMIT_SHA/`git rev-parse origin/feature/k8s`/g" nginx-routing/deployment-template.yaml > nginx-routing/deployment.yaml
+sed -e "s/COMMIT_SHA/`git rev-parse origin/feature/k8s`/g" novels-api/deployment-template.yaml > novels-api/deployment.yaml
+sed -e "s/COMMIT_SHA/`git rev-parse origin/feature/k8s`/g" senteces-api/deployment-template.yaml > senteces-api/deployment.yaml
+sed -e "s/COMMIT_SHA/`git rev-parse origin/feature/k8s`/g" novelists-api/deployment-template.yaml > novelists-api/deployment.yaml
